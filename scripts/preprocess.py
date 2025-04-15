0bc9545253d0a79177cfa442b6225d02574a5262
Nikita Karpov <91084477+nikarpoff@users.noreply.github.com>
2025-04-15 15:05:11 +0300

    feat: add basic dataset preprocessing

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -13,12 +13,44 @@
 # limitations under the License.
 
 
+import os
 import pandas as pd
+from argparse import ArgumentParser
 
-# Absolute path to the dataset.
-DATASET_PATH = "F:\\dataset\\music\\autotagging_moodtheme.tsv"
+OUTPUTS_PATH = "./outputs"
 
-def load_dataset(dataset_path=DATASET_PATH) -> pd.DataFrame:
+
+def cli_arguments_preprocess() -> tuple:
+    """
+    Read, parse and preprocess command line arguments:
+        - path to source dataset. Required
+        - number of moods. By default: all moods (0)
+
+    Sorce dataset file should be named "autotagging_moodtheme.tsv"
+    """
+    parser = ArgumentParser(description="Preprocessing autotagging moods dataset script. Cleans moods, aggregates them and saves preprocessed .tsv file")
+    filename = "autotagging_moodtheme.tsv"
+
+    parser.add_argument("--path", required=True,
+                      help="Path to source dataset")
+    
+    parser.add_argument("--moods", required=False,
+                      choices=["2", "4", "all"],
+                      help="Number of aggregated moods. By default: all source moods")
+
+    args = parser.parse_args()
+
+    if args.path[-1] != "/":
+        args.path += "/"
+    
+    args.path += filename
+
+    if not args.moods or args.moods == "all":
+        args.moods = 0
+
+    return args.path, args.moods
+
+def load_dataset(dataset_path: str) -> pd.DataFrame:
     """
     Reads the dataset from the specified path and returns it as a pandas DataFrame.
     Handles cases where the last column contains multiple values separated by tabs.
@@ -43,28 +75,97 @@ def load_dataset(dataset_path=DATASET_PATH) -> pd.DataFrame:
         print(f"An error occurred: {e}")
         return None
 
-def clean_last_column(df: pd.DataFrame) -> pd.DataFrame:
+def clean_target(df: pd.DataFrame) -> pd.DataFrame:
     """
     Cleans the last column of the DataFrame by removing the prefix 'mood/theme---'
-    and replacing tab characters with commas.
+    and splites multi tags by tabs
     """
-    if df is not None and not df.empty:
-        last_column = df.columns[-1]
-        df[last_column] = df[last_column].str.replace("mood/theme---", "", regex=False)
-        df[last_column] = df[last_column].str.replace("\t", ",", regex=False)
+    last_column = df.columns[-1]
+    df[last_column] = df[last_column].str.replace("mood/theme---", "", regex=False)
+    df[last_column] = df[last_column].str.split("\t")
     return df
 
-if __name__ == "__main__":
-    dataset = load_dataset()
+def clean_columns_name(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Refactors the column names to lower case
+    """
+    columns = df.columns
+    refactored_columns = columns.str.lower()
+    df.columns = refactored_columns
+    return df
 
-    if dataset is not None:
+def clean_id_columns(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Cleans the ID columns by removing the prifix "track", "artist", "album" and parses them to int
+    Remember that this code cleanes first three columns: "track_id", "artist_id", "album_id" in such order
+    """
+    track_id_column = df.columns[0]
+    artist_id_column = df.columns[1]
+    album_id_column = df.columns[2]
+
+    df[track_id_column] = df[track_id_column].str.replace("track_", "", regex=False)
+    df[artist_id_column] = df[artist_id_column].str.replace("artist_", "", regex=False)
+    df[album_id_column] = df[album_id_column].str.replace("album_", "", regex=False)
+
+    df[track_id_column] = pd.to_numeric(df[track_id_column])
+    df[artist_id_column] = pd.to_numeric(df[artist_id_column])
+    df[album_id_column] = pd.to_numeric(df[album_id_column])
+
+    return df
+
+def clean_dataset_pipeline(dataset: pd.DataFrame) -> pd.DataFrame:
+    """
+    Cleans the dataset by applying all cleaning functions in order.
+    """
+    df = dataset.copy()  # Avoid modifying the original DataFrame
+    df = clean_columns_name(df)
+    df = clean_target(df)
+    df = clean_id_columns(df)
+    return df
+
+def merge_moods(dataset: pd.DataFrame, merge_mode: int) -> pd.DataFrame:
+    pass
+
+def main(outputs_path):
+    dataset_path, moods_merge_mode = cli_arguments_preprocess()
+
+    if not os.path.exists(OUTPUTS_PATH):
+        os.mkdir(OUTPUTS_PATH)
+
+    dataset = load_dataset(dataset_path)
+    
+    if dataset is not None and not dataset.empty:
         print(f"Dataset loaded successfully with {len(dataset)} rows and {len(dataset.columns)} columns.")
-        print(dataset.head(n=10))
+        print(dataset.head(n=10), "\n")
 
-        clean_last_column(dataset)
+        cleaned_dataset = clean_dataset_pipeline(dataset)
 
-        print("\nLast column cleaned.")
-        print(dataset.head(n=10))
+        print("Dataset cleaned successfully:")
+        print(cleaned_dataset.head(n=10), "\n")
+        cleaned_dataset.info()
 
+        tags_distribution_save_path = os.path.join(OUTPUTS_PATH, "tags_distribution.csv")
+        tags_distribution = cleaned_dataset["tags"].explode().value_counts()
+        tags_distribution.to_csv(tags_distribution_save_path, index=True)
+        print("Tags distribution saved successfully. Path: ", tags_distribution_save_path)
+
+        if moods_merge_mode == 0:
+            save_path = os.path.join(dataset_path, "dataset_all_moods.tsv")
+            cleaned_dataset.to_csv(save_path, sep="\t", index=False)
+            print("Dataset saved successfully. Path: ", save_path)
+            return
+
+        final_dataset = merge_moods(cleaned_dataset, moods_merge_mode)
+
+        if moods_merge_mode == 2:
+            save_path = os.path.join(dataset_path, "dataset_2_moods.tsv")
+            
+        final_dataset.to_csv(save_path, sep="\t", index=False)
+        print("Dataset saved successfully. Path: ", save_path)
     else:
         print("Failed to load dataset.")
+
+
+if __name__ == "__main__":
+    main(outputs_path=OUTPUTS_PATH)
+
