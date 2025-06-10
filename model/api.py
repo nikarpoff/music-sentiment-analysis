fix: fix scaling and gpu inference
+ fix scaling
+ gpu preprocessing in model inference (torchaudio instead of the librosa)

model/api.py
@@ -7,7 +7,6 @@ import numpy as np
 from datetime import datetime
 from modelarch import *
 from fastapi import FastAPI, File, UploadFile
-from sklearn.preprocessing import MinMaxScaler
 
 import utils
 
@@ -15,7 +14,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, 'data')
 
 MODEL_NAME_HS = "specstr_hs_13M.pth"
-MODEL_NAME_RE = "specstr_re_8M.pth"
+MODEL_NAME_RE = "specstr_re_TEST.pth"
 
 CLASSES_JSON_NAME_HS = f"classes_hs.json"
 CLASSES_JSON_NAME_RE = f"classes_re.json"
@@ -41,8 +40,7 @@ classes_hs = json.load(open(os.path.join(DATA_PATH, CLASSES_JSON_NAME_HS), 'r'))
 classes_re = json.load(open(os.path.join(DATA_PATH, CLASSES_JSON_NAME_RE), 'r'))
 classes = classes_hs + classes_re
 
-scaler = MinMaxScaler(feature_range=(0, 1))
-
+specs_processor = utils.SpectrogramProcessor(device="cuda")
 
 @app.post("/api/model/predict")
 async def predict(file: UploadFile = File(...)):
@@ -50,9 +48,7 @@ async def predict(file: UploadFile = File(...)):
     audio_bytes = await file.read()
 
     # Form mel-spectrogram
-    spec = utils.get_mel_spec(audio_bytes)
-    spec = scaler.fit_transform(spec)
-    spec = torch.from_numpy(spec).float().unsqueeze(0)
+    spec = specs_processor.pipeline(audio_bytes)
 
     # Log current time.
     start_time = datetime.now()
@@ -68,7 +64,6 @@ async def predict(file: UploadFile = File(...)):
 
     # Form and send response.
     probabilities = np.concatenate((hs_probs, re_probs), axis=0)
-    print(probabilities)
     prediction = classes[probabilities.argmax()]
     
     return {"predict": prediction, "probs": probabilities.tolist(), "classes": classes}

model/utils.py
@@ -1,58 +1,90 @@
 import torch
 from torch import nn
-import librosa
+import torch.functional as F
+import torchaudio
 import numpy as np
 import io
 
 
-# Use params like in MTG-Jamendo dataset.
-FFT_WINDOW_LEN = 512
-HOP_LEN = 256
-WINDOW = 'hann'
-N_MELS = 96
+class SpectrogramProcessor:
+    """
+    Functionality to load and preprocess mel-spectrograms due torch.
+    Use the same params for mel-spec generation as in target MTG-Jamendo dataset.
+    """
+    def __init__(self,
+                 target_sample_rate=12000,
+                 fft_window_len=512,
+                 hop_len=256,
+                 n_mels=96,
+                 min_db_value=-90.0,
+                 max_db_value=40.,
+                 inference_window_len: int = 4096,
+                 inference_hop_len: int = 2048,
+                 inference_max_hops: int = 8,
+                 device="cuda"):
+        self.target_sample_rate = target_sample_rate
+        self.min_db_value = min_db_value
+        self.max_db_value = max_db_value
+        self.window_len = inference_window_len
+        self.hop_len = inference_hop_len
+        self.max_hops = inference_max_hops
+        self.device = device
 
-# Length of spectrogram to model observe
-SPEC_WINDOW_LEN = 4096
-SPEC_HOP_LEN = 2048
-MAX_HOPS = 8
+        self.mel_transform = torchaudio.transforms.MelSpectrogram(
+            sample_rate=target_sample_rate,
+            n_fft=fft_window_len,
+            hop_length=hop_len,
+            n_mels=n_mels,
+            window_fn=torch.hann_window,
+        ).to(device)
+        self.db_transform = torchaudio.transforms.AmplitudeToDB(stype="power").to(device)
 
-def get_mel_spec(audio_bytes):
-    # Read as binary buffer.
-    audio_buffer = io.BytesIO(audio_bytes)
+    def pipeline(self, audio_bytes):
+        spec = self.get_mel_spec(audio_bytes)
+        spec = self.minmax_scale_tensor(spec)
+        return self.preprocess_spectrogram(spec)
 
-    # Upload file by librosa. Use the same sample rate as in dataset.
-    target_sr = 12000
-    y, sr = librosa.load(audio_buffer, sr=target_sr)
-    # Get spectrogram.
-    mel_spec = librosa.feature.melspectrogram(y=y,
-                                              sr=sr,
-                                              n_fft=FFT_WINDOW_LEN,
-                                              hop_length=HOP_LEN,
-                                              n_mels=N_MELS,
-                                              window=WINDOW,)
-    mel_spec_db = librosa.power_to_db(mel_spec, ref=np.max)
+    def get_mel_spec(self, audio_bytes):
+        waveform, sr = torchaudio.load(io.BytesIO(audio_bytes), normalize=True)
+        if sr != self.target_sample_rate:
+            waveform = torchaudio.functional.resample(waveform, sr, self.target_sample_rate).to(self.device)
+        
+        spec = self.mel_transform(waveform)
+        spec_db = self.db_transform(spec)
 
-    print(np.min(mel_spec_db))
-    print(np.max(mel_spec_db))
+        return spec_db
 
-    return mel_spec_db
+    def minmax_scale_tensor(self, spec_db: torch.Tensor) -> torch.Tensor:
+        spec_db = spec_db.clamp(min=self.min_db_value, max=self.max_db_value)
+        return (spec_db - self.min_db_value) / (self.max_db_value - self.min_db_value)
+
+    def preprocess_spectrogram(self, spec: np.array,) -> torch.Tensor:
+        """
+        Batch the mel-spectrogram throught windows.
+        """
+        mels_n, length = spec.size(1), spec.size(-1)
+
+        print(torch.min(spec), torch.max(spec), "\n")
+
+        # Pad if needed
+        if length < self.window_len:
+            spec = F.pad(spec, (0, self.window_len - length))
+            length = self.window_len
+
+        # Compute number of windows
+        num = min(self.max_hops, 1 + (length - self.window_len) // self.hop_len)
+
+        # Create strided tensor of shape (num, mels_n, window_len)
+        strides = spec.stride()
+        windowed = spec.as_strided(
+            size=(num, mels_n, self.window_len),
+            stride=(self.hop_len * strides[-1], strides[-2], strides[-1])
+        )
+
+        return windowed
 
 def inference(model: nn.Module, spec: torch.Tensor) -> np.array:
-    current_step = 0
-    current_timestamp = 0
-    total_timestamps = spec.shape[-1]
-
-    probabilities = []
-
     with torch.no_grad():
-        while current_timestamp < total_timestamps and current_step < MAX_HOPS:
-            windowed_spec = spec[:, :, current_timestamp:current_timestamp + SPEC_WINDOW_LEN]
-            probabilities.append(torch.softmax(model(windowed_spec), dim=1).cpu().numpy().flatten())
-            current_timestamp += SPEC_HOP_LEN
-            current_step += 1
-    
-    if len(probabilities) == 0:
-        print("Audio too short to predict!")
-        return np.array[0., 0.]
+        probabilities = torch.softmax(model(spec), dim=1).cpu().numpy()
 
     return np.mean(probabilities, axis=0)

training/main.py
@@ -92,8 +92,8 @@ def get_specs_scaler(melspecs_stats_path) -> MinMaxScaler:
         with open(melspecs_stats_path) as file:
             stats = json.load(file)
 
-            min_amplitude = stats["max_amplitude"]
-            max_amplitude = stats["min_amplitude"]
+            min_amplitude = stats["min_amplitude"]
+            max_amplitude = stats["max_amplitude"]
     else:
         print("Warning! For MinMaxScaler max/min amplitudes from melspecs_stats.json required! Now using min/max defaults values.")
         print("You can run scripts.stats to get required statistics and avoid this warning.")
@@ -104,11 +104,17 @@ def get_specs_scaler(melspecs_stats_path) -> MinMaxScaler:
     # Average audio can have -90 min amplitude and +29.6 max amplitude. So melspecs should be scaled. 
     scaler = MinMaxScaler(feature_range=(0, 1))
     # Cause fitting MinMaxScaler is too expencieve, params can be set manually
-    scaler.min_ = np.array([0])
-    scaler.scale_ = np.array([1 / (max_amplitude - min_amplitude)])
-    scaler.data_min_ = np.array([min_amplitude])
-    scaler.data_max_ = np.array([max_amplitude])
-    scaler.data_range_ = np.array([max_amplitude - min_amplitude])
+    data_min  = np.array([min_amplitude])
+    data_max  = np.array([max_amplitude])
+    data_range = data_max - data_min
+    scale_ = (scaler.feature_range[1] - scaler.feature_range[0]) / data_range
+    min_   = scaler.feature_range[0] - data_min * scale_
+
+    scaler.scale_ = scale_
+    scaler.min_ = min_
+    scaler.data_min_ = data_min
+    scaler.data_max_ = data_max
+    scaler.data_range_ = data_range
 
     return scaler, min_amplitude, max_amplitude
 
@@ -172,7 +178,7 @@ if __name__ == "__main__":
         # Load the dataset.
         kfold_dataloader = KFoldSpecsDataLoader(dataset_path, dataset_name, kfold_splits, target_mode, pad_value=min_amp,
                                                 batch_size=batch_size, max_seq_len=max_seq_len, min_seq_len=min_seq_len, 
-                                                use_augmentation=True, num_workers=4, test_size=0.2, outputs_path=outputs_path,
+                                                use_augmentation=True, num_workers=8, test_size=0.2, outputs_path=outputs_path,
                                                 transform_specs=transform_specs, moods=moods, random_state=random_state)
     elif model_type in RAW_AUDIO_MODELS or model_type in LIRYCS_MODELS:  # raw audio models or audio->lirycs models required raw audio data loader
         max_seq_len = int(os.getenv(f"{model_type.upper()}_MAX_SEQ_LEN", 50000))
@@ -197,7 +203,7 @@ if __name__ == "__main__":
     if model_type == SPECSTR:
         dropout = float(os.getenv("SPECSTR_DROPOUT", 0.2))
 
-        model = SpectrogramTransformer(output_dim=output_dim, dropout=dropout, device=device).to(device)
+        model = SpectrogramSmallTransformer(output_dim=output_dim, dropout=dropout, device=device).to(device)
     elif model_type == SPECS_AUTOENCODER:
         dropout = float(os.getenv("SPECS_AUTOENCODER_DROPOUT", 0.2))
 

training/utils/utils.py
@@ -300,9 +300,9 @@ class ClassificationModelTrainer(ModelTrainer):
             raise ValueError(f"Target mode {target_mode} not available in ClassificationModelTrainer")
         
         if data_mode == "audiospecs":
-            self.model_call = self._audio_spec_model_call
+            self.model_call = audio_spec_model_call
         elif data_mode == "default":
-            self.model_call = self._default_model_call
+            self.model_call = default_model_call
         else:
             raise ValueError(f"Unknown data_model: {data_mode}")
 
@@ -333,7 +333,7 @@ class ClassificationModelTrainer(ModelTrainer):
             self.optimizer.zero_grad()
 
             with torch.amp.autocast("cuda"):
-                outputs = self.model_call(inputs)
+                outputs = self.model_call(self.model, inputs)
                 loss = self.loss_function(outputs, labels)
 
             # Scaled Backward Pass and gradient Clipping
@@ -381,7 +381,7 @@ class ClassificationModelTrainer(ModelTrainer):
             for i, data in enumerate(loader):
                 inputs, labels = data
                 labels = labels.to(self.model.device, non_blocking=True).long()
-                outputs = self.model_call(inputs)
+                outputs = self.model_call(self.model, inputs)
                 loss = self.loss_function(outputs, labels)
 
                 running_loss += loss